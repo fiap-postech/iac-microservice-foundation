3c9bfc972a021387a869b034a6b2b03be6b3ff81
Eberson Oliveira <eberson.olivera@gmail.com>
2024-01-24 21:41:10 -0300

    added auth endpoint to gateway

--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
 output "apigw_endpoint" {
-  value = aws_apigatewayv2_api.apigw_http_endpoint.api_endpoint
+  value = {
+    endpoint = aws_apigatewayv2_api.apigw_http_endpoint.api_endpoint
+    id       = aws_apigatewayv2_api.apigw_http_endpoint.id
+  }
 }
